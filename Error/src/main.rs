6e67ea3a77d27dcc5ef906dfc9ff6709bf8edd9b
Bishnu Prasad Rath <rbishnu604@gmail.com>
2026-08-24 16:38:32 +0530

    Learning about Error Handling

---
+++ Error/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+    panic!("This is my panic"); //It will prevent to excute the RUST program further and htis is used for error handling in RUST
+    //Panic gives a unrecoverable error
+    println!("This is the end of the program")
+}
